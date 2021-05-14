a5c24e09ba25e74e2a9bccb6fbaae6a055b89916
Allen Royston <allen.acr@gmail.com>
2021-05-14 16:49:52 +0200

    updated

--- src/Apps/ComponentLibrary/components/_accordion.svelte
+++ src/Apps/ComponentLibrary/components/_accordion.svelte
@@ -13,12 +13,13 @@
   `
   export let open = false
   export let fill = false
-
-`} />
+  export let rounded = false;
+  export let outline = false;
+  `} />
 
 
 <LibraryBlock title='Default:'>
-  <Accordian rounded outline>
+  <Accordian>
     <div slot='title'>
       <h3>Accordion</h3>
     </div>
@@ -31,7 +32,7 @@
 
 
 <CodeBlock open title='Example:' snippet={`
-  <Accordian rounded outline>
+  <Accordian>
     <div slot='title'>
       <h3>Accordion</h3>
     </div>
@@ -40,7 +41,47 @@
       <LoremBlock />
     </div>
   </Accordian>
+  `} />  
 
-`} />  
+
+<LibraryBlock title='Outline:'>
+  <Accordian outline>
+    <div slot='title'>
+      <h3>Accordion</h3>
+    </div>
+
+    <div slot='content'>
+      <LoremBlock />
+    </div>
+  </Accordian>
+</LibraryBlock>
+
+
+<CodeBlock open title='Example:' snippet={`
+  <Accordian outline>
+    ...
+  </Accordian>
+  `} />  
+
+
+<LibraryBlock title='Rounded:'>
+  <Accordian outline rounded>
+    <div slot='title'>
+      <h3>Accordion</h3>
+    </div>
+
+    <div slot='content'>
+      <LoremBlock />
+    </div>
+  </Accordian>
+</LibraryBlock>
+
+
+<CodeBlock open title='Example:' snippet={`
+  <Accordian outline rounded>
+    ...
+  </Accordian>
+  `} />  
+
 
   
