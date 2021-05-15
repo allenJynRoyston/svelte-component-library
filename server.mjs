47120139a5ec02ddb0dad17da89e843236d014a7
Allen Royston <allen.acr@gmail.com>
2021-05-15 23:37:23 +0200

    updated

--- server.mjs
+++ server.mjs
@@ -5,5 +5,7 @@ import fastifyStatic from "fastify-static";
 const app = fastify({ logger: false });
 app.register(fastifyStatic, { root: join(resolve(), "build") });
 
-app.listen(3000);
-console.log("Now serving on localhost:3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port);
+console.log(`Now serving on localhost: ${port}`);
