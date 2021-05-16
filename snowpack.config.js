a75366b39e7971b5fc29dd1a29737c2579645bae
Allen Royston <allen.acr@gmail.com>
2021-05-16 12:04:33 +0200

    updated

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,67 +1,72 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
+const isDev = process.env.NODE_ENV === "development";
+
 module.exports = {
   mount: {
     public: "/",
     src: "/_dist_",
   },
-  plugins: [
-    // FIRST, BUNDLE THE PROJECT WITH ROLLUP
-    [
-      "snowpack-plugin-rollup-bundle",
-      {
-        emitHtmlFiles: true,
-        preserveSourceFiles: false,
-        entrypoints: "build/index.js",
-      },
-    ],
-    // COPY ASSET FOLDERS *find a better method for this*
-    [
-      "snowpack-plugin-copy",
-      {
-        patterns: [
+  plugins: isDev
+    ? ["@snowpack/plugin-svelte"]
+    : [
+        // CONVERT SVELTE TO ROLLOUT FORMAT
+        "@snowpack/plugin-svelte",
+        // FIRST, BUNDLE THE PROJECT WITH ROLLUP
+        [
+          "snowpack-plugin-rollup-bundle",
           {
-            source: "public/images/bg/**",
-            destination: "build/images/bg/",
-          },
-          {
-            source: "public/images/emotions/**",
-            destination: "build/images/emotions/",
-          },
-          {
-            source: "public/images/favicon/**",
-            destination: "build/images/favicon/",
-          },
-          {
-            source: "public/images/logo/**",
-            destination: "build/images/logo/",
-          },
-          {
-            source: "public/images/profile/**",
-            destination: "build/images/profile/",
-          },
-          {
-            source: "public/manifest.json",
-            destination: "build/",
+            emitHtmlFiles: true,
+            preserveSourceFiles: false,
+            entrypoints: "build/index.js",
           },
         ],
-      },
-    ],
-    // OPTIMIZE FILES/FOLDERS
-    ["snowpack-plugin-optimize"],
-    // OPTIMIZE IMAGES
-    [
-      "snowpack-plugin-imagemin",
-      {
-        /* see "Plugin Options" below */
-        include: ["**/*.jpg", "**/*.png"],
-        plugins: [
-          require("imagemin-mozjpeg")({ quality: 90, progressive: true }),
-          require("imagemin-optipng")({ optimizationLevel: 7 }),
+        // COPY ASSET FOLDERS *find a better method for this*
+        [
+          "snowpack-plugin-copy",
+          {
+            patterns: [
+              {
+                source: "public/images/bg/**",
+                destination: "build/images/bg/",
+              },
+              {
+                source: "public/images/emotions/**",
+                destination: "build/images/emotions/",
+              },
+              {
+                source: "public/images/favicon/**",
+                destination: "build/images/favicon/",
+              },
+              {
+                source: "public/images/logo/**",
+                destination: "build/images/logo/",
+              },
+              {
+                source: "public/images/profile/**",
+                destination: "build/images/profile/",
+              },
+              {
+                source: "public/manifest.json",
+                destination: "build/",
+              },
+            ],
+          },
         ],
-      },
-    ],
-    "@snowpack/plugin-svelte",
-  ],
+        // OPTIMIZE FILES/FOLDERS
+        ["snowpack-plugin-optimize"],
+        // OPTIMIZE IMAGES
+        [
+          "snowpack-plugin-imagemin",
+          {
+            /* see "Plugin Options" below */
+            include: ["**/*.jpg", "**/*.png"],
+            plugins: [
+              require("imagemin-mozjpeg")({ quality: 90, progressive: true }),
+              require("imagemin-optipng")({ optimizationLevel: 7 }),
+            ],
+          },
+        ],
+      ],
   install: [
     /* ... */
   ],
@@ -80,6 +85,5 @@ module.exports = {
   alias: {
     "@lib": "./src/Apps/ComponentLibrary/components",
     "@components": "./src/components",
-    "@scss": "./src/scss/src",
   },
 };
