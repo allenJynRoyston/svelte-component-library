4f77f21d8f1a6621d7baf5cb5815b4dc01a84cb3
Allen Royston <allen.acr@gmail.com>
2021-05-16 09:28:57 +0200

    updated

--- snowpack.config.js
+++ snowpack.config.js
@@ -8,41 +8,19 @@ module.exports = {
     [
       "@snowpack/plugin-webpack",
       {
-        htmlMinifierOptions: true,
-        sourceMap: false,
-        collapseWhitespace: true,
-        removeComments: true,
-        removeEmptyAttributes: true,
-        removeRedundantAttributes: true,
-        removeScriptTypeAttributes: true,
-        removeStyleLinkTypeAttributes: true,
+        /* webpack options */
+        // htmlMinifierOptions: true,
+        // sourceMap: false,
+        // collapseWhitespace: true,
+        // removeComments: true,
+        // removeEmptyAttributes: true,
+        // removeRedundantAttributes: true,
+        // removeScriptTypeAttributes: true,
+        // removeStyleLinkTypeAttributes: true,
       },
     ],
     "@snowpack/plugin-svelte",
   ],
-  optimization: {
-    splitChunks: {
-      chunks: "async",
-      minSize: 20000,
-      minRemainingSize: 0,
-      minChunks: 1,
-      maxAsyncRequests: 30,
-      maxInitialRequests: 30,
-      enforceSizeThreshold: 50000,
-      cacheGroups: {
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10,
-          reuseExistingChunk: true,
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true,
-        },
-      },
-    },
-  },
   install: [
     /* ... */
   ],
@@ -61,5 +39,6 @@ module.exports = {
   alias: {
     "@lib": "./src/Apps/ComponentLibrary/components",
     "@components": "./src/components",
+    "@scss": "./src/scss/src",
   },
 };
